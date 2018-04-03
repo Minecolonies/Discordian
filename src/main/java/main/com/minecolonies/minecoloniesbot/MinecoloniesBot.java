853d0b650be402eba2b63b9fb88762c85cf14539
Unknown <Asher.parry@icloud.com>
2018-04-03 16:34:13 +1000

    init

---
+++ src/main/java/main/com/minecolonies/minecoloniesbot/MinecoloniesBot.java
@@ -0,0 +1,10 @@
+package main.com.minecolonies.minecoloniesbot;
+
+public class MinecoloniesBot
+{
+    public static void main(String[] args)
+    {
+        System.out.println("test123");
+        System.out.println("Testing123456");
+    }
+}
